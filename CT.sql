d1d807ba90b67c954f373b570d11ec727df46667
SwagPanda <antoine.blondeau@isen-bretagne.fr>
2016-04-06 22:36:36 +0200

    Ajout Fichier de création de la base de donnée

--- CT.sql
+++ CT.sql
@@ -5,23 +5,14 @@ create table AUTEUR
 	age 		 INTEGER,
 	sexe 		 VARCHAR(5),
 	codepostal 	 INTEGER,
-	autorisation INTEGER(1)
+	autorisation     INTEGER(1)
 )engine=innodb;
 create table PUBLICATION
 (
-	id 			VARCHAR(10)				PRIMARY KEY,
+	id 		VARCHAR(10)		PRIMARY KEY,
 	texte 		VARCHAR(140),	
 )engine=innodb;
 create table CATEGORIE
 (
 	activite 	VARCHAR(25) 		PRIMARY KEY
 )engine=innodb;
-
-alter table COMMANDE add FOREIGN KEY(numclient) REFERENCES CLIENT(numclient);
-alter table COMMANDE_PRODUIT 
-	add FOREIGN KEY	(numcde) 	REFERENCES COMMANDE (numcde),
-	add FOREIGN KEY	(reference) REFERENCES PRODUIT(reference);
-alter table FACTURE add FOREIGN KEY (numcde)	REFERENCES COMMANDE (numcde);
-alter table FACTURE_PRODUIT
-	add FOREIGN KEY (numfact)			REFERENCES FACTURE (numfact),
-	add FOREIGN KEY (reference) 		REFERENCES PRODUIT (reference);
